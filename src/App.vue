ffe237ca238c72cba7bacc5c8ca667bce048fba0
jinchenxing <2541226493@qq.com>
2023-10-27 10:49:14 +0800

    4.0

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue 33333.0" />
+  <HelloWorld msg="Vite + Vue 4444.0" />
 </template>
 
 <style scoped>
